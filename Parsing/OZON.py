03c90e6231e301bbaf03c1a4747fdf09da5c385d
thegreatmindgorb <proxximo169@gmail.com>
2025-04-12 21:10:09 +0300

    OZON parser: copy from video, not working yet

--- Parsing/OZON.py
+++ Parsing/OZON.py
@@ -1,7 +1,51 @@
-import requests
+import json
+import time
+import undetected_chromedriver as uc
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-URL = "https://www.ozon.ru/product/samsung-smartfon-galaxy-s23-ultra-sm-s918n100-podderzhka-russkogo-yazyka-1sim-karta-12-512-1758923870/?at=PjtJzrWB8cJJ8G34I7M7JWYTn61qOgsnnZLLgTYwG2N&keywords=samsung+s23+ultra"
-response = requests.get(URL)
-soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.prettify())
+#функция списана из видео -- пока не работает, видимо на озоне что-то поменяли с тех пор
+def get_products_links(item_name='каска туристическая'):
+    driver = uc.Chrome()
+    driver.implicitly_wait(5)
+    driver.get(url='https://ozon.ru')
+    time.sleep(2)
+
+    find_input = driver.find_element(By.NAME, 'text')
+    find_input.clear()
+    find_input.send_keys(item_name)
+    time.sleep(2)
+
+    find_input.send_keys(Keys.ENTER)
+    time.sleep(2)
+
+    current_url = f'{driver.current_url}'
+    driver.get(url=current_url)
+    time.sleep(2)
+
+    try:
+        find_links = driver.find_elements(By.CLASS_NAME,'tile-hover-target')
+        products_urls = list(set([f'{link.get_attribute("href")}' for link in find_links]))
+        print('Ссылки на товары собраны')
+        print(products_urls)
+    except Exception:
+        products_urls = []
+        print('Что-то пошло не так')
+
+    products_dict = {}
+    for k, v in enumerate(products_urls):
+        products_dict.update({k:v})
+
+    with open('products_urls_dict.json', 'w', encoding='utf-8') as f:
+        json.dump(products_dict, f, indent=4, ensure_ascii=False)
+    driver.close()
+    driver.quit()
+
+def main():
+    get_products_links()
+
+if __name__ == '__main__':
+    main()
