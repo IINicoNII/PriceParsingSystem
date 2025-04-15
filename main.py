0e2f34c0caaf94ac5c82f91361fdf6bb471eb910
Vladimir <vikont1509@gmail.com>
2025-04-15 20:45:38 +0300

    Обновленные данные в BManager and main.py

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from DataBase.DBManager import DBManager
 
 if __name__ == "__main__":
     db_manager = DBManager()
-    db_manager.update_product(1937862493)
-    #db_manager.update_product(339875290)
+    #db_manager.update_product(1937862493)
+    db_manager.update_product(1821495135)
